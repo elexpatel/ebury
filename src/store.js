d4084536389949c3e8cc1cb0cf4c99a6cc78a225
Elex Patel <elexpatel@gmail.com>
2019-11-14 10:51:59 +0000

    vuex structure for composer form and confirmation

--- src/store.js
+++ src/store.js
@@ -4,7 +4,41 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    composerForm: {
+      emailTo: [], // made into array to allow more flexibility when sending data;
+      emailCC: [],
+      emailBCC: [],
+      emailSubject: "",
+      emailMessage: "",
+      emailAttachements: []
+    },
+    composerConformation: false
+  },
+  actions: {
+    updateComposer($event, payload) {
+      $event.commit("UPDATE_COMPOSER", payload);
+    },
+    sendComposer($event) {
+      // this is where an AJAX POST request would be preformed to the backend API.
+      $event.commit("UPDATE_COMPOSER_CONFORMATION", true);
+    }
+  },
+  mutations: {
+    UPDATE_COMPOSER(state, payload) {
+      console.log(state);
+      console.log(payload);
+    },
+    UPDATE_COMPOSER_CONFORMATION(state, status) {
+      state.composerConformation = status;
+    }
+  },
+  getters: {
+    composerFrom(state) {
+      return state.composerForm;
+    },
+    composerConfirmation(state) {
+      return state.composerConformation;
+    }
+  }
 });
