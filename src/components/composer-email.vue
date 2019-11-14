01df1934b276f15417adeff6745ae715510d0f84
Elex Patel <elexpatel@gmail.com>
2019-11-14 12:02:17 +0000

    get first unit test to pass

--- src/components/composer-email.vue
+++ src/components/composer-email.vue
@@ -5,22 +5,28 @@
         <legend class="form__legend">Send E-mail</legend>
         <ul class="form__list">
           <li class="form__list-item">
-            <input type="email" class="form__field" placeholder="To" />
+            <label for="composer-to-email" class="form__label">To</label>
+            <input v-model="composerTo" id="composer-to-email" type="email" class="form__field" placeholder="To" />
           </li>
           <li class="form__list-item">
-            <input type="email" class="form__field" placeholder="CC" />
+            <label for="composer-cc-email" class="form__label">CC</label>
+            <input v-model="composerCC" id="composer-cc-email" type="email" class="form__field" placeholder="CC" />
           </li>
           <li class="form__list-item">
-            <input type="email" class="form__field" placeholder="BCC" />
+            <label for="composer-bcc-email" class="form__label">BCC</label>
+            <input v-model="composerBCC" id="composer-bcc-email" type="email" class="form__field" placeholder="BCC" />
           </li>
           <li class="form__list-item">
-            <input v-model="composerSubject" type="text" class="form__field" placeholder="Subject" />
+            <label for="composer-subject" class="form__label">Subject</label>
+            <input v-model="composerSubject" id="composer-subject" type="text" class="form__field" placeholder="Subject" />
           </li>
           <li class="form__list-item">
-            <textarea class="form__field" placeholder="Message"></textarea>
+            <label for="composer-message" class="form__label">Message</label>
+            <textarea v-model="composerMessage" id="composer-message" class="form__field" placeholder="Message"></textarea>
           </li>
           <li class="form__list-item--hidden">
-            <input type="file" class="form__field" />
+            <label for="composer-attachment">Attachment</label>
+            <input @change="composerAttachment" id="composer-attachment" type="file" class="form__field" />
           </li>
         </ul>
       </fieldset>
@@ -38,12 +44,67 @@ export default {
     composerForm() {
       return this.$store.getters.composerFrom;
     },
+    composerTo: {
+      get() {
+        return this.composerForm.emailTo.length ? this.composerForm.emailTo.join(","): "";
+      },
+      set(value) {
+        if (value.indexOf(",") !== -1) {
+          value = value.split(",");
+        } else {
+          value = [value.trim()];
+        }
+        this.$store.dispatch("updateComposer", { emailTo: value });
+      }
+    },
+    composerCC: {
+      get() {
+        return this.composerForm.emailCC.length ? this.composerForm.emailCC.join(","): "";
+      },
+      set(value) {
+        if (value.indexOf(",") !== -1) {
+          value = value.split(",");
+        } else {
+          value = [value.trim()];
+        }
+        this.$store.dispatch("updateComposer", { emailCC: value });
+      }
+    },
+    composerBCC: {
+      get() {
+        return this.composerForm.emailBCC.length ? this.composerForm.emailBCC.join(","): "";
+      },
+      set(value) {
+        if (value.indexOf(",") !== -1) {
+          value = value.split(",");
+        } else {
+          value = [value.trim()];
+        }
+        this.$store.dispatch("updateComposer", { emailBCC: value });
+      }
+    },
     composerSubject: {
       get() {
         return this.composerForm.emailSubject;
       },
       set(value) {
         this.$store.dispatch("updateComposer", { emailSubject: value });
+      }
+    },
+    composerMessage: {
+      get() {
+        return this.composerForm.emailMessage;
+      },
+      set(value) {
+        this.$store.dispatch("updateComposer", { emailMessage: value });
+      }
+    },
+    composerAttachment: {
+      get() {
+        return this.composerForm.emailMessage;
+      },
+      set(value) {
+        this.$store.dispatch("updateComposer", { emailMessage: value });
       }
     }
   }
--- tests/unit/composer-email.spec.js
+++ tests/unit/composer-email.spec.js
@@ -1,77 +1,108 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import ComposerEmail from "@/components/composer-email.vue";
+
+const localVue = createLocalVue();
+
+let createConfig = overrides => {
+  const mocks = {
+    $store: {
+      getters: {
+        composerFrom: {
+          emailTo: [],
+          emailCC: [],
+          emailBCC: [],
+          emailSubject: "",
+          emailMessage: "",
+          emailAttachements: []
+        }
+      },
+      commit: () => {},
+      dispatch: () => {}
+    }
+  };
+
+  return Object.assign({ mocks, localVue }, overrides);
+};
 
 describe("composer-email.vue", () => {
   it("should return all fields empty", () => {
-    const wrapper = shallowMount(ComposerEmail);
+    let config = createConfig();
+    const wrapper = shallowMount(ComposerEmail, config);
 
     // need to change these to check data values rather than inspecting the DOM (quicker check, and allows design tweaks without effecting business logic)
+    expect(wrapper.find("#composer-to-email").element.value).toBe("");
+    expect(wrapper.find("#composer-cc-email").element.value).toBe("");
+    expect(wrapper.find("#composer-bcc-email").element.value).toBe("");
+    expect(wrapper.find("#composer-subject").element.value).toBe("");
+    expect(wrapper.find("#composer-message").element.value).toBe("");
+    expect(wrapper.find("#composer-attachment").element.value).toBe("");
 
-    expect(wrapper.find("#composer-to-email").value).toBe(""); // make fail on setup
-    expect(wrapper.find("#composer-cc-email").value).toBe(""); // make fail on setup
-    expect(wrapper.find("#composer-bcc-email").value).toBe(""); // make fail on setup
-    expect(wrapper.find("#composer-subject").value).toBe(""); // make fail on setup
-    expect(wrapper.find("#composer-message").value).toBe(""); // make fail on setup
-    expect(wrapper.find("#composer-attachement").value).toBe(""); // make fail on setup
+    // inspect data values (quicker)
+    expect(wrapper.vm.composerTo).toBe("");
+    expect(wrapper.vm.composerCC).toBe("");
+    expect(wrapper.vm.composerBCC).toBe("");
+    expect(wrapper.vm.composerSubject).toBe("");
+    expect(wrapper.vm.composerMessage).toBe("");
+    expect(wrapper.vm.composerAttachment).toBe("");
   });
 
-  it("should invalidate 'To' field if incorrect email format", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-to-email").value).toBe("false"); // make fail on setup
-  });
+  // it("should invalidate 'To' field if incorrect email format", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-to-email").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should allow multiple valid email addresses in 'To' field", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-to-email").value).toBe("false"); // make fail on setup
-  });
+  // it("should allow multiple valid email addresses in 'To' field", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-to-email").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should invalidate 'CC' field if incorrect email format", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-cc-email").value).toBe("false"); // make fail on setup
-  });
+  // it("should invalidate 'CC' field if incorrect email format", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-cc-email").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should allow multiple valid email addresses in 'CC' field", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-cc-email").value).toBe("false"); // make fail on setup
-  });
+  // it("should allow multiple valid email addresses in 'CC' field", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-cc-email").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should invalidate 'BCC' field if incorrect email format", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-bcc-email").value).toBe("false"); // make fail on setup
-  });
+  // it("should invalidate 'BCC' field if incorrect email format", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-bcc-email").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should allow multiple valid email addresses in 'BCC' field", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-bcc-email").value).toBe("false"); // make fail on setup
-  });
+  // it("should allow multiple valid email addresses in 'BCC' field", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-bcc-email").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should invalidate 'Subject' field if no value entered", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-subject").value).toBe("false"); // make fail on setup
-  });
+  // it("should invalidate 'Subject' field if no value entered", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-subject").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should invalidate 'Message' field if no value entered", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-message").value).toBe("false"); // make fail on setup
-  });
+  // it("should invalidate 'Message' field if no value entered", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-message").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should show preview of attached files", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-attachment").value).toBe("false"); // make fail on setup
-  });
+  // it("should show preview of attached files", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-attachment").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should remove specific attached file upon user event", () => {
-    const wrapper = shallowMount(ComposerEmail);
-    expect(wrapper.find("#composer-attachment").value).toBe("false"); // make fail on setup
-  });
+  // it("should remove specific attached file upon user event", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
+  //   expect(wrapper.find("#composer-attachment").value).toBe("false"); // make fail on setup
+  // });
 
-  it("should only show active submit button when all required fields are valid", () => {
-    const wrapper = shallowMount(ComposerEmail);
+  // it("should only show active submit button when all required fields are valid", () => {
+  //   const wrapper = shallowMount(ComposerEmail);
 
-    expect(wrapper.find("#composer-to-email").value).toBe("mock@email.com");
-    expect(wrapper.find("#composer-subject").value).toBe("Mock subject");
-    expect(wrapper.find("#composer-message").value).toBe("Mock message");
+  //   expect(wrapper.find("#composer-to-email").value).toBe("mock@email.com");
+  //   expect(wrapper.find("#composer-subject").value).toBe("Mock subject");
+  //   expect(wrapper.find("#composer-message").value).toBe("Mock message");
 
-    expect(wrapper.find("#composer-submit").value).toBe("false"); // make fail on setup
-  });
+  //   expect(wrapper.find("#composer-submit").value).toBe("false"); // make fail on setup
+  // });
 });
