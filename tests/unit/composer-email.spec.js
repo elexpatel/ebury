6fbdab6d84d6bb1dad3e7697064e357dd57e57d3
Elex Patel <elexpatel@gmail.com>
2019-11-14 10:20:35 +0000

    add initial unit test for email composer - should all fail

--- tests/unit/composer-email.spec.js
+++ tests/unit/composer-email.spec.js
@@ -14,4 +14,64 @@ describe("composer-email.vue", () => {
     expect(wrapper.find("#composer-message").value).toBe(""); // make fail on setup
     expect(wrapper.find("#composer-attachement").value).toBe(""); // make fail on setup
   });
+
+  it("should invalidate 'To' field if incorrect email format", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-to-email").value).toBe("false"); // make fail on setup
+  });
+
+  it("should allow multiple valid email addresses in 'To' field", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-to-email").value).toBe("false"); // make fail on setup
+  });
+
+  it("should invalidate 'CC' field if incorrect email format", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-cc-email").value).toBe("false"); // make fail on setup
+  });
+
+  it("should allow multiple valid email addresses in 'CC' field", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-cc-email").value).toBe("false"); // make fail on setup
+  });
+
+  it("should invalidate 'BCC' field if incorrect email format", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-bcc-email").value).toBe("false"); // make fail on setup
+  });
+
+  it("should allow multiple valid email addresses in 'BCC' field", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-bcc-email").value).toBe("false"); // make fail on setup
+  });
+
+  it("should invalidate 'Subject' field if no value entered", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-subject").value).toBe("false"); // make fail on setup
+  });
+
+  it("should invalidate 'Message' field if no value entered", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-message").value).toBe("false"); // make fail on setup
+  });
+
+  it("should show preview of attached files", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-attachment").value).toBe("false"); // make fail on setup
+  });
+
+  it("should remove specific attached file upon user event", () => {
+    const wrapper = shallowMount(ComposerEmail);
+    expect(wrapper.find("#composer-attachment").value).toBe("false"); // make fail on setup
+  });
+
+  it("should only show active submit button when all required fields are valid", () => {
+    const wrapper = shallowMount(ComposerEmail);
+
+    expect(wrapper.find("#composer-to-email").value).toBe("mock@email.com");
+    expect(wrapper.find("#composer-subject").value).toBe("Mock subject");
+    expect(wrapper.find("#composer-message").value).toBe("Mock message");
+
+    expect(wrapper.find("#composer-submit").value).toBe("false"); // make fail on setup
+  });
 });
